387d89acc99bff9b6d07ad2db6902ad46067fa68
R_S_9 <rafik.saakyan.2016@mail.ru>
2022-03-26 18:49:58 +0700

    add user setting

---
+++ core/app_users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class EmailBackend(ModelBackend):
+    """Class Clients"""
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        """Авторизация пользователя"""
+        user_model = get_user_model()
+
+        try:
+            user = user_model.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except user_model.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        """Получения объекта пользователя"""
+        user_model = get_user_model()
+
+        try:
+            return user_model.objects.get(pk=user_id)
+        except user_model.DoesNotExist:
+            return None
---
+++ core/app_users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+urlpatterns = [
+    # Users
+    # path('user/', None),
+]
