40b827bad2797342eae58e28db8c2ffcf4ebe04a
R_S_9 <rafik.saakyan.2016@mail.ru>
2022-05-13 21:22:27 +0700

    del info list

--- src/mainapp/urls.py
+++ src/mainapp/urls.py
@@ -5,7 +5,6 @@ from .search_result.resource import SearchResult
 from .sto_map.resource import STOMap
 from .review.resource import ReviewResource
 from .cooperation.resource import Cooperation
-from .info_list_live.resource import InfoListView
 
 
 urlpatterns = [
@@ -17,5 +16,4 @@ urlpatterns = [
         'service-map/review/', ReviewResource.as_view(), name='create_review'
     ),
     path('cooperation/', Cooperation.as_view(), name='cooperation_map'),
-    path('info_list/', InfoListView.as_view(), name='info_list'),
 ]
